ebfee15fea62caaf82fc35f54e906d6228499bad
inferman <infernal.manowar@gmail.com>
2019-09-09 14:24:09 +0300

    configured webpack

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,7 +1,25 @@
 import path from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+import fs from "fs";
 
+function generateHtmlPlugins(templateDir) {
+  // Read files in template directory
+  const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
+  return templateFiles.map(item => {
+    // Split names and extension
+    const parts = item.split(".");
+    const name = parts[0];
+    const extension = parts[1];
+    // Create new HTMLWebpackPlugin with options
+    return new HtmlWebpackPlugin({
+      filename: `${name}.html`,
+      template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`)
+    });
+  });
+}
+
+const htmlPlugins = generateHtmlPlugins("./src/pages");
 
 export default {
   entry: path.join(__dirname, "src/index.js"),
@@ -55,7 +73,7 @@ export default {
     new MiniCssExtractPlugin({
       filename: "bundle.css"
     })
-  ],
+  ].concat(htmlPlugins),
   stats: "minimal",
   devtool: "source-map",
   mode: "development",
