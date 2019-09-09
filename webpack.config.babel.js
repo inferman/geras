b0a8b3f077c474eb09f78518d4002d7d933bf9fa
inferman <infernal.manowar@gmail.com>
2019-09-09 14:54:59 +0300

    added links to pages

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,14 +4,11 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 import fs from "fs";
 
 function generateHtmlPlugins(templateDir) {
-  // Read files in template directory
   const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
   return templateFiles.map(item => {
-    // Split names and extension
     const parts = item.split(".");
     const name = parts[0];
     const extension = parts[1];
-    // Create new HTMLWebpackPlugin with options
     return new HtmlWebpackPlugin({
       filename: `${name}.html`,
       template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`)
