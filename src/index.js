fde11f9e0469168b8d4be45f98a1e59c2b655a59
inferman <infernal.manowar@gmail.com>
2019-09-11 16:19:50 +0300

    added color palette

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import "./../assets/img/icons/search-icon.svg";
 import "./../assets/img/icons/arrow-down.svg";
 import "./../assets/img/icons/download-arrow-down.svg";
 import "./../assets/img/icons/check-mark.svg";
+import "./../assets/img/icons/doctor-icon.svg";
 
 import "./../assets/js/sidebarToggle";
 import "./../assets/js/selector";
