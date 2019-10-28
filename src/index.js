bfc0fd2d3cdcaa49e8b095e62f14404e0df4808b
inferman <infernal.manowar@gmail.com>
2019-10-28 13:37:18 +0200

    diagnosyic questions completed

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import "./../assets/img/icons/check-mark.svg";
 import "./../assets/img/icons/doctor-icon.svg";
 import "./../assets/img/icons/notes-icon.svg";
 import "./../assets/img/icons/clock-icon.svg";
+import "./../assets/img/icons/play-icon.svg";
 
 import "./../assets/js/sidebarToggle";
 import "./../assets/js/selector";
